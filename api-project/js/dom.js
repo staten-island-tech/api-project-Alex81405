52d0808ce55b8e639105f188cb45982e621f873b
AlexASC21 <87743123+Alex81405@users.noreply.github.com>
2023-02-01 01:41:43 -0500

    API Project

--- api-project/js/dom.js
+++
@@ -1,7 +0,0 @@
-const DOMSelectors = {
-    form: document.querySelector(".form"),
-    box: document.querySelector(".box"),
-    userInput: document.getElementById("userInput"),
-  };
-  
-  export {DOMSelectors};
--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,67 +1,49 @@
-import "../styles/style.css";
-import { DOMSelectors } from "./dom.js";
+import "/styles/style.css";
 
-const url = "https://api.adoptapet.com/search/pets_at_shelter?key=A34F48&v=1&output=xml&shelter_id=2342";
-
-async function getData(url) {
-  try {
-    const response = await fetch(url);
-    if (response.status < 200 || response.status > 299) {
-      throw new Error(response);
-    } else {
-      const data = await response.json();
-      console.log(data);
-      console.log("good");
-    }
-
-  } catch (error) {
-    console.log(error);
-    console.log("bad");
-  }
-}
-
-getData(url);
-
-DOMSelectors.form.addEventListener("submit", function (e) {
-  e.preventDefault();
-  console.log(e);
-  let userInput = DOMSelectors.userInput.value;
-
-async function specificCharacter(url, userInput) {
-    try{
-      const response = await fetch(url);
-      const data = await response.json();
-      data.data
-      .filter((element) => element.name.includes(`${userInput}`))
-      .map((element) => {
-        DOMSelectors.box.insertAdjacentHTML(
-        //   "beforeend",
-        //   `<div class="card">
-        //   <img class="image" src="${element.imageUrl}" alt="">
-        //   <h2 class="name">${element.name}</h2>
-        //   <div class="features">
-        //     <ul><b>Films: </b> ${element.films}</ul>
-        //     <br></br>
-        //     <ul><b>Short Films: </b> ${element.shortFilms}</ul>
-        //     <br></br>
-        //     <ul><b>TV Shows: </b>${element.tvShows}</ul>
-        //     <br></br>
-        //     <ul><b>Video Games: </b> ${element.videoGames}</ul>
-        //   </div>
-        // </div>
-        // `
-        )
-      })
-    }catch (error) {
-      console.log(error);
-    }
-  }
-  specificCharacter(url, userInput);
-})
-
-function clear() {
-  DOMSelectors.box.innerHTML = "";
-  DOMSelectors.userInput.innerHTML = "";
+const DOMSelectors = {
+    jokeButton: document.getElementById("getJokeButton"),
+    joke: document.getElementById("joke"),
+    searchButton: document.getElementById("searchButton"),
+    searchInput: document.getElementById("searchInput")
 };
 
-clear()
+const URL = "https://v2.jokeapi.dev/joke/Any?safe-mode";
+
+async function getJoke(url) {
+    try {
+        const response = await fetch(url)
+        if (response.status < 200 || response.status > 299) {
+            console.log(response.status);
+            throw new Error(response);
+        } else {
+            const data = await response.json();
+            console.log(data);
+            if (data.setup) {
+                DOMSelectors.joke.innerHTML = `${data.setup} <br><br> ${data.delivery}`;
+            } else if (data.error === true) {
+                DOMSelectors.joke.innerHTML = data.causedBy;
+            }
+            else {
+                DOMSelectors.joke.innerHTML = data.joke;
+            }
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+DOMSelectors.searchButton.addEventListener("click", function () {
+    let newURL = URL + "&contains=" + DOMSelectors.searchInput.value;
+    console.log(searchInput.value)
+    console.log(newURL)
+    getJoke(newURL);
+    DOMSelectors.searchInput.value = "";
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    getJoke(URL);
+});
+
+DOMSelectors.jokeButton.addEventListener("click", function () {
+    getJoke(URL);
+});
